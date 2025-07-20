604876d0bbf4f2b7637bf83092ab7b86d997cde4
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 15:25:35 -0500

    final changes with notes

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -29,17 +29,47 @@ const readline = require('readline-sync');
 const hasTorch = true;
 const hasMap = false;
 
+const isTame = true; 
+// const scaredOfHorses = true;
+
 console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+const choice = readline.question("Do you go to the 'mountains' or the 'village'? ");
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
+  console.log("After passing through the mountains, you come upon a wild horse.");
+  const choiceTwo = readline.question("Do you try to 'tame' it or 'continue' on without it? ");
+    if (choiceTwo === "tame" && isTame) {
+      console.log("You manage to tame the horse and you can now ride it.");
+      } else if (choiceTwo === "tame" && !isTame) {
+        console.log("The horse rears up, spooked, and injurs you");
+      }
+      else console.log("You continue on without the horse.");
+    }
+ else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
 } else if (choice === "village" || hasMap) {
   console.log("You find your way to the village.");
 } else {
   console.log("You get lost and wander aimlessly.");
+}
+/*
+const isTame = true; 
+const scaredOfHorses = true;
+
+if (choice === mountains && hasTorch) {
+  console.log("After passing through the mountains, you come upon a wild horse.");
+  const choiceTwo = readline.question("Do you try to 'tame' it or 'continue' on without it?");
+
+  if (choiceTwo === "tame" && isTame) {
+    console.log("You manage to tame the horse and you can now ride it.");
+  } else if (choiceTwo === "tame" && !isTame) {
+    console.log("The horse rears up, spooked, and injurs you");
+  } else if (choiceTwo === "continue" || scaredOfHorses) {
+    console.log("You are scared of horses and quiety continue on.");
+  } else {
+    console.log("You keep going - no time for horses.");
+  }
 }
 
 /* 
